c3b9bef2aea215822a50ce9cd8197f829ecae93e
Vinícius Cortez <vinisicius@gmail.com>
2021-05-10 19:28:19 -0300

    grafico_win

--- Lab2/q1.py
+++ Lab2/q1.py
@@ -1,24 +1,36 @@
 """
 Objetivo: Resolver questão 1 do segundo laboratorio.
 """
-import sympy
+
 from math import exp
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def conta(a):
-    e = exp(1)  # exponencial de 1
-    if a <= 0:
-        I = (1 / e) * (e - 1)  # I0
-        return I
-    else:
-        I_novo = 1 - (1 / (a + 1)) * conta(a - 1)  # Recursiva para tratar o I(n+1)
-        return I_novo
+def f(n):   #Calcula o valor de um I passado
+    e = exp(1)
+    I = (1 / e) * (e - 1)   #I0
+    soma = 0
+    for c in range(0, n + 1):   #Calcula cada I ate In de maneira sucessiva
+        if c == 0:
+            I_n = I
+        else:
+            I_n = 1 - (1 / (c + 1)) * soma
+        soma = I_n
+    # ao final temos In = soma
+    return soma
 
 
-n = sympy.Symbol('n')
-# n = float()
-l = sympy.limit(conta(n), n, 0)
-f = 1 / n
-g = sympy.limit(f, n, 0)
-# sympy.plot(conta(n), xlim=[-10, 10], ylim=[-10, 10])
-print(g)
+x = np.arange(0, 301, 1)    #Gera array de 0 a 300
+y = [f(i) for i in x]       #Gera lista de f(0) a f(300)
+
+#Layout do grafico
+plt.style.use('ggplot')
+plt.figure(figsize=(7, 5))
+plt.xlabel('Valores de n')
+plt.ylabel('Valores de I')
+#Gera o grafico
+plt.plot(x, y)
+plt.tight_layout()
+#Plota o grafico
+plt.show()
