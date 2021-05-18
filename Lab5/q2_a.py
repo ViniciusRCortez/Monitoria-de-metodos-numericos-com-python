71af785ba000f8662b84c92560e7145352868fc3
Vinícius Cortez <vinisicius@gmail.com>
2021-05-18 09:49:19 -0300

    newton-modificado

--- Lab5/q2_a.py
+++ Lab5/q2_a.py
@@ -4,16 +4,17 @@ Objetivo: Resolver questão 2a do quinto laboratorio.
 import numpy as np
 from Funcao_q2 import FuncQ2  # ver arquivo Funcao_q2
 
-# USANDO NEWTON-RAPHSON
+# USANDO NEWTON-RAPHSON MODIFICADO
 x_1 = [1.0]  # listas com os valores encontrados de x
 x_2 = [1.0]
 x_3 = [-1.0]
+func = FuncQ2(x_1[-1], x_2[-1], x_3[-1])
+#No metodo mdificado, calculamos o jacobiano somente uma vez
+jacobiano = np.array([[func.da_x(), func.da_y(), func.da_z()], #derivadas da eq_1 aplicada ao primeiro (x1,x2,x3)
+                      [func.db_x(), func.db_y(), func.db_z()], #derivadas da eq_2 aplicada ao primeiro (x1,x2,x3)
+                      [func.dc_x(), func.dc_y(), func.dc_z()]]) #derivadas da eq_3 aplicada ao primeiro (x1,x2,x3)
 while True:
     func = FuncQ2(x_1[-1], x_2[-1], x_3[-1])
-    #matrizes
-    jacobiano = np.array([[func.da_x(), func.da_y(), func.da_z()], #derivadas da eq_1 aplicada ao ultimo (x1,x2,x3)
-                          [func.db_x(), func.db_y(), func.db_z()], #derivadas da eq_2 aplicada ao ultimo (x1,x2,x3)
-                          [func.dc_x(), func.dc_y(), func.dc_z()]]) #derivadas da eq_3 aplicada ao ultimo (x1,x2,x3)
     funcoes = np.array([[-func.a()],      #eq_1 aplicada ao ultimo (x1,x2,x3)
                         [-func.b()],      #eq_2 aplicada ao ultimo (x1,x2,x3)
                         [-func.c()]])     #eq_3 aplicada ao ultimo (x1,x2,x3)
@@ -29,4 +30,4 @@ while True:
     if erro_x1 < 10**(-6) and erro_x2 < 10**(-6) and erro_x3 < 10**(-6):
         break
 
-print(f'Pelo método de Newton-Raphson as três funções se cruzam no ponto:\n({x_1[-1]}, {x_2[-1]}, {x_3[-1]})')
+print(f'Pelo método de Newton-Raphson modificado as três funções se cruzam no ponto:\n({x_1[-1]}, {x_2[-1]}, {x_3[-1]})')
