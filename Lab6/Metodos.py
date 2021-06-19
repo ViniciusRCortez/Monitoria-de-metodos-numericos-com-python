a70b46afa6a6f8fd4aff7fd37562060744ea6b8e
Vinícius Cortez <vinisicius@gmail.com>
2021-06-19 17:22:34 -0300

    Lab6

--- Lab6/Metodos.py
+++ Lab6/Metodos.py
@@ -4,26 +4,58 @@ Implementação dos metodos de interpolação de Newton e de Lagrange.
 import numpy as np
 
 
-def Pol_Newton(x, y, p):
+def Pol_Newton(x, y, po, pf):
     """
     Implementação do metodo de interpolação de Newton.
     :param x: Vetor com os valores de X.
     :param y: Vetor com os valores de Y.
-    :param p: O valor de X onde se pretende calcular a aproximação.
-    :return: O valor da aproximação de Y.
+    :param po: O valor inicial de X onde se pretende calcular a aproximação.
+    :param pf: O valor final de X onde se pretende calcular a aproximação.
+    :return: Dicionario com os valores entre os ponto pedidos e suas respectivas aproximações de F(x).
     """
-    #P(x) = d0 + d1(x-x0) + d2(x-x0)(x-x1).....
     tam = len(x)
-    FDD = np.ones(tam**2).reshape((tam, tam))*-1#Tabela de diferenças divididas(começa em -1 para visualização)
-    for i in range(tam):
-        FDD[i][0] = y[i]                        #Primeira coluna
-    for i in range(1, tam):
-        for j in range(tam-i):                  #Demais colunas
-            FDD[j][i] = (FDD[j+1][i-1] - FDD[j][i-1])/(x[j+i] - x[j])    #Faz todas as colunas antes de iterar a linha
-    x_termo = 1                                 #Auxiliar para a multiplicaçãos pelos termos de X
-    pol = FDD[0][0]                             #Polinomio de Newton
-    for ordem in range(1, tam):
-        x_termo = x_termo*(p - x[ordem-1])
-        pol += FDD[0][ordem]*x_termo            #Operador(primeiro termo de cada coluna)*multiplicação dos x
-    return pol
+    res = np.array([], dtype='float32')
+    pontos = np.linspace(po, pf, 100)       #Intervalo a ser calculado
+    for p in pontos:
+        FDD = np.ones(tam**2).reshape((tam, tam))*-1#Tabela de diferenças divididas(começa em -1 para visualização)
+        for i in range(tam):
+            FDD[i][0] = y[i]                        #Primeira coluna
+        for i in range(1, tam):
+            for j in range(tam-i):                  #Demais colunas
+                FDD[j][i] = (FDD[j+1][i-1] - FDD[j][i-1])/(x[j+i] - x[j])    #Faz todas as colunas antes de iterar a linha
+        x_termo = 1                                 #Auxiliar para a multiplicaçãos pelos termos de X
+        pol = FDD[0][0]                             #Polinomio de Newton
+        for ordem in range(1, tam):
+            x_termo = x_termo*(p - x[ordem-1])
+            pol += FDD[0][ordem]*x_termo            #Operador(primeiro termo de cada coluna)*multiplicação dos x
+        res = np.append(res, pol)
+    resultado = {"X": pontos,
+                 "Y": res}
+    return resultado
 
+
+def Pol_Lagrange(x, y, po, pf):
+    """
+    Implementação do metodo de interpolação de Lagrande.
+    :param x: Vetor com os valores de X.
+    :param y: Vetor com os valores de Y.
+    :param po: O valor inicial de X onde se pretende calcular a aproximação.
+    :param pf: O valor final de X onde se pretende calcular a aproximação.
+    :return: Dicionario com os valores entre os ponto pedidos e suas respectivas aproximações de F(x).
+    """
+    res = np.array([], dtype='float32')
+    r = 0
+    tam = len(x)
+    pontos = np.linspace(po, pf, 100)  # Intervalo a ser calculado
+    for ponto in pontos:
+        for i in range(0, tam):         #Cada ordem de y(i)L(i)
+            produtorio = 1              #L(i)
+            for j in range(0, tam):
+                if j != i:
+                    produtorio *= (ponto - x[j])/(x[i] - x[j])
+            r += produtorio*y[i]
+        res = np.append(res, r)
+        r = 0
+    resultado = {"X": pontos,
+                 "Y": res}
+    return resultado
--- Lab6/Questão.py
+++ Lab6/Questão.py
@@ -2,20 +2,37 @@
 Resolução das questões do Lab6.
 """
 import numpy as np
-from Metodos import Pol_Newton
+from Metodos import Pol_Newton, Pol_Lagrange
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 5, dtype='float16')    #Pontos de X
+x = np.arange(0, 5.1, 1, dtype='float16')    #Pontos de X
 f = lambda c: c**2                      #F(x)
 y = np.array([], dtype='float16')       #Pontos de Y
 pol = np.array([], dtype='float16')     #Pontos da aproximação
 for i in x:
     y = np.append(y, f(i))
 
-for i in range(len(x)):
-    r = Pol_Newton(x, y, i)
-    pol = np.append(pol, r)
-plt.plot(x, y, 'r-')
-plt.plot(x+1, pol, 'g--')
+#Para o Metodo de Newton:
+pol = Pol_Newton(x, y, 0, 5)
+plt.plot(pol["X"], pol["Y"], 'r-', label='Valores aproximados')
+plt.plot(x, y, 'bx', label='Valores medidos')
+plt.xlabel("Valores de X")
+plt.ylabel('Valores de F(x)')
+plt.title('Grafico de interpolação de Newton')
+plt.style.use('ggplot')
+plt.tight_layout()
+plt.legend(loc='best')
+plt.grid()
 plt.show()
-"""CONTINUAR COM A LOGICA PROPOSTA"""
+
+#Para o Metodo de Lagrange:
+pol = Pol_Lagrange(x, y, 0, 5)
+plt.plot(pol["X"], pol["Y"], 'r-', label='Valores aproximados')
+plt.plot(x, y, 'bx', label='Valores medidos')
+plt.xlabel("Valores de X")
+plt.ylabel('Valores de F(x)')
+plt.title('Grafico de interpolação de Lagrange')
+plt.style.use('ggplot')
+plt.tight_layout()
+plt.legend(loc='best')
+plt.show()
